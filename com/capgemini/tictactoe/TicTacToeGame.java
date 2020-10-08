60488c0cdbf1d1a8f8fcbea26b5610c97eb8e9d4
unknown <rachitshukla08@gmail.com>
2020-10-08 08:58:14 +0530

    Added: TicTacToeGame class and initialized a board

---
+++ com/capgemini/tictactoe/TicTacToeGame.java
@@ -0,0 +1,12 @@
+package com.capgemini.tictactoe;
+
+public class TicTacToeGame {
+	
+	public static void main(String[] args) {
+		
+		char[] boardElement = new char[10];
+		for(int i=0;i<boardElement.length ; i++) {
+			boardElement[i] = ' ';
+		}
+	}
+}
