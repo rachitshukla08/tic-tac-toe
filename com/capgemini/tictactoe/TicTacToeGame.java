4411d2a45a29b67989da72a2a8b9ef9911626b9c
unknown <rachitshukla08@gmail.com>
2020-10-08 10:12:27 +0530

    Added: getInput method to take input and determine whether input is by player or computer

--- com/capgemini/tictactoe/TicTacToeGame.java
+++ com/capgemini/tictactoe/TicTacToeGame.java
@@ -1,20 +1,45 @@
 package com.capgemini.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	/**
+	 * UC1
+	 * 
 	 * @return boardElement array with empty board
 	 */
-	public char[] createBoard() {
-		char[] boardElement = new char[10];
-		for (int index = 1; index < boardElement.length; index++) {
-			boardElement[index] = ' ';
+	private char[] createBoard() {
+		char[] board = new char[10];
+		for (int index = 1; index < board.length; index++) {
+			board[index] = ' ';
 		}
-		return boardElement;
+		return board;
+	}
+
+	/**
+	 * UC2
+	 * 
+	 * @return Whether input is by player or computer
+	 */
+	private String getInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter X or O");
+		char input = scanner.nextLine().charAt(0);
+		scanner.close();
+		if (input == 'X')
+			return "Input is entered by player";
+		else if (input == 'O')
+			return "Input is entered by computer";
+		else
+			return "Invalid Input";
 	}
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to TicTacToe game");
 		TicTacToeGame ticTacToe = new TicTacToeGame();
-		char[] boardElement = ticTacToe.createBoard();
+		char[] board = ticTacToe.createBoard();
+		String inputIsBy = ticTacToe.getInput();
+		System.out.println(inputIsBy);
 	}
 }
